8df55b82906141258de56f9ba09104c2cd958d85
Khoa <minhkhoav47@gmail.com>
2026-03-11 19:15:18 +0700

    sua lai cau 2

--- 25520884_BTTH_BT2.cpp
+++ 25520884_BTTH_BT2.cpp
@@ -5,97 +5,106 @@ struct Phanso{
     int tuso;
     int mauso;
 
-    // hàm UCLN: tìm ước chung lớn nhất của |a| và |b|
-    // đầu vao: 2 số nguyên a,b;
-    // đầu ra: UCLN(|a|,|b|)
-    int UCLN(int a,int b){
-        return __gcd(abs(a),abs(b)); 
+    // Ham UCLN
+    // Dau vao: 2 so nguyen a, b
+    // Dau ra : uoc chung lon nhat cua |a| va |b|
+    // Chuc nang: dung __gcd() ket hop abs() de xu li so am
+    int UCLN(int a, int b){
+        return __gcd(abs(a), abs(b));
         // ta lấy __gcd có sẵn trong thư viện <bits/stdc++.h>
         // sử dụng abs xử lí trường hợp nếu a hoặc b âm
     }
+
+    // Ham rutgon
     // hàm rutgon(): rút gọn phân số về dạng tối giản 
     // đầu vào: Lấy tuso và mauso có trong hàm struct
     // đầu ra: Thay đỏi trực tiếp tuso và mauso
+    // Chuc nang: chia tu va mau cho UCLN, dam bao mau luon duong
     void rutgon(){
-        int g = UCLN(tuso,mauso); 
-        tuso /= g;
-        mauso /= g; 
+        int g = UCLN(tuso, mauso);
+        tuso  /= g;
+        mauso /= g;
         if(mauso < 0){
-            tuso = -tuso; 
+            tuso  = -tuso;
             mauso = -mauso;
         }
     }
-    // hàm nhập xuất phân số 
-    // đầu vào: use nhập tuso và mauso
+
+    // Ham nhapps
+    // hàm nhập phân số 
+    // đầu vào: user nhập tuso và mauso
     // đầu ra: gán giá trị ngược về struct và rút gọn laị
+    // Chuc nang: vong lap kiem tra sai kieu du lieu va mau = 0
     void nhapps(){
-        int t,m;
+        int t, m;
         do{
-            cout<<"nhap gia tri cho tu so va mau so";
-            cin>>t>>m;
-            if(cin.fail()){ 
-                // cin.fail() để kiểm tra sai đinh dạng kiểu dữ liệu của tử và mẫu (nhập chuỗi, số thập phân)
-                cout<<"nhap sai dinh dang, yeu cau nhap lai"<<endl;
+            cout << "Nhap tu so va mau so: ";
+            cin >> t >> m;
+            if(cin.fail()){
+                cout << "Sai dinh dang, yeu cau nhap lai!" << endl;
                 cin.clear();
-                // su dung cin.clear() để tắt đi trạng thái fail của máy tinh giúp user nhập lại input
-                cin.ignore(1000,'\n'); 
-                // cin.ignore() xóa bộ nhớ đệm hay dữ liệu cũ 
-                // tránh dữ liệu bị đè lên nhau 
+                cin.ignore(1000, '\n');
                 continue;
             }
             if(m == 0){
-                cout<<"mau khong duoc bang khong"<<endl;
+                cout << "Mau khong duoc bang 0!" << endl;
                 continue;
             }
-            break; 
-        }while(true); 
-        tuso = t; 
+            break;
+        }while(true);
+        tuso  = t;
         mauso = m;
-        rutgon(); 
+        rutgon();
     }
+
+    // Ham bangnhau
+    // Dau vao: phan so p2
+    // Dau ra : true neu p1 == p2, false neu khac
+    // Chuc nang: so sanh truc tiep tuso va mauso sau khi da rut gon
     bool bangnhau(Phanso p2){
-        //Đầu vào: Phân số p2
-        //Đầu ra: true nếu bằng nhau, false nếu khác
-        //Chức năng: So sánh trực tiếp tuso và mauso sau khi đã rút gọn
         return tuso == p2.tuso && mauso == p2.mauso;
     }
-    bool sosanh(Phanso p2){
-        //Đầu vào: Phân số p2
-        //Đầu ra: true nếu p1 > p2, false nếu p1 < p2
-        //Chức năng: Nhân chéo tuso * p2.mauso so với mauso * p2.tuso
-        int left = tuso * p2.mauso;
-        int right = mauso * p2.tuso;
-        if(left > right){
-            return true;
-        }
-        else return false;
-    }
-    void xuatps(){
-        // hàm xuatps
-        // Đầu vao: Không có
-        // đầu ra: in tuso/mauso ra màn hình output
-        cout<<tuso<<"/"<<mauso;
-    }    
-};
-int main(){
-    Phanso p1,p2;
-    cout<<"nhap thanh phan cho phan so p1"<<endl;
-    p1.nhapps();
-    cout<<"nhap thanh phan cho phan so p2:"<<endl;
-    p2.nhapps();
-    cout << "Ket qua: Phan so lon nhat la: ";
 
-    if (p1.bangnhau(p2)) { 
-        p1.xuatps();
-        cout << " (hai phan so bang nhau)"<<endl;
+    // Ham sosanh
+    // Dau vao: phan so p2
+    // Dau ra : true neu p1 > p2, false neu p1 < p2
+    // Chuc nang: nhan cheo (tuso * p2.mauso) voi (mauso * p2.tuso)
+    bool sosanh(Phanso p2){
+        long long left  = (long long)tuso  * p2.mauso;
+        long long right = (long long)mauso * p2.tuso;
+        return left > right;
     }
-    else if (p1.sosanh(p2)) {
-        p1.xuatps();
-        cout<<endl;
+
+    // Ham xuatps
+    // Đầu vao: Không có
+    // đầu ra: in tuso/mauso ra màn hình output
+    // Chuc nang: hien thi phan so, mau luon duong do rutgon() dam bao
+    void xuatps(){
+        cout << tuso << "/" << mauso;
     }
-    else {
+};
+
+int main(){
+    Phanso p1, p2;
+
+    cout << "Nhap phan so p1:" << endl;
+    p1.nhapps();
+    cout << "Nhap phan so p2:" << endl;
+    p2.nhapps();
+
+    cout << "Ket qua: ";
+    if(p1.bangnhau(p2)){
+        cout << "Hai phan so bang nhau: ";
+        p1.xuatps();
+    }
+    else if(p1.sosanh(p2)){
+        cout << "Phan so lon nhat la: ";
+        p1.xuatps();
+    }
+    else{
+        cout << "Phan so lon nhat la: ";
         p2.xuatps();
-        cout <<endl;
     }
+    cout << endl;
     return 0;
 }
