869c369645486c052adc6d3326dc853a5bfdf416
Khoa <minhkhoav47@gmail.com>
2026-03-11 13:48:14 +0700

    sua lai cau 3

--- 25520884_BTTH_BT3.cpp
+++ 25520884_BTTH_BT3.cpp
@@ -5,11 +5,17 @@ struct Phanso{
     int tuso;
     int mauso;
 
-
-
+    // Ham UCLN
+    // Dau vao: 2 so nguyen a, b
+    // Dau ra : uoc chung lon nhat cua |a| va |b|
+    // Chuc nang: dung __gcd() ket hop abs() de xu li so am
     int UCLN(int a,int b){
         return __gcd(abs(a),abs(b)); 
     }
+    // Ham rutgon
+    // Dau vao: tuso, mauso cua struct
+    // Dau ra : khong co, thay doi truc tiep tuso va mauso
+    // Chuc nang: chia tu va mau cho UCLN, dam bao mau luon duong
     void rutgon(){
         int g = UCLN(tuso,mauso); 
         tuso /= g;
@@ -19,7 +25,10 @@ struct Phanso{
             mauso = -mauso;
         }
     }
-
+    // Ham nhapps
+    // Dau vao: nguoi dung nhap tu so va mau so tu ban phim
+    // Dau ra : khong co, gan gia tri vao struct va rut gon lai
+    // Chuc nang: vong lap kiem tra sai kieu du lieu va mau = 0
     void nhapps(){
         int t,m;
         do{
@@ -46,6 +55,10 @@ struct Phanso{
         mauso = m;
         rutgon(); 
     }
+    // Ham tong
+    // Dau vao: phan so p2
+    // Dau ra : phan so ket qua p1 + p2 da rut gon
+    // Chuc nang: tinh (tuso*p2.mauso + p2.tuso*mauso) / (mauso*p2.mauso)
     Phanso tong(Phanso p2){
         Phanso res;
         res.tuso = tuso * p2.mauso + mauso * p2.tuso;
@@ -53,6 +66,10 @@ struct Phanso{
         rutgon();
         return res;
     }
+    // Ham hieu
+    // Dau vao: phan so p2
+    // Dau ra : phan so ket qua p1 - p2 da rut gon
+    // Chuc nang: tinh (tuso*p2.mauso - p2.tuso*mauso) / (mauso*p2.mauso)
     Phanso hieu(Phanso p2){
         Phanso res;
         res.tuso = tuso * p2.mauso - mauso * p2.tuso;
@@ -60,6 +77,10 @@ struct Phanso{
         rutgon();
         return res;
     }
+    // Ham tich
+    // Dau vao: phan so p2
+    // Dau ra : phan so ket qua p1 * p2 da rut gon
+    // Chuc nang: tinh (tuso*p2.tuso) / (mauso*p2.mauso)
     Phanso tich(Phanso p2){
         Phanso res;
         res.tuso = tuso * p2.tuso;
@@ -67,6 +88,10 @@ struct Phanso{
         rutgon();
         return res;
     }
+    // Ham thuong
+    // Dau vao: phan so p2 (p2 khac 0)
+    // Dau ra : phan so ket qua p1 / p2 da rut gon
+    // Chuc nang: nhan p1 voi nghich dao cua p2, kiem tra p2 khac 0
     Phanso thuong(Phanso p2){
         Phanso res;
         res.tuso = tuso * p2.mauso;
@@ -74,6 +99,10 @@ struct Phanso{
         rutgon();
         return res;
     }
+    // Ham xuatps
+    // Dau vao: khong co
+    // Dau ra : in phan so dang "tuso/mauso" ra man hinh
+    // Chuc nang: hien thi phan so, mau luon duong do rutgon() dam bao
     void xuatps(){
         cout<<tuso<<"/"<<mauso;
     }    
