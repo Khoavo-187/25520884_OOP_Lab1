b103a63de8cad415150f340e4ae97a99c7b7b261
Khoa <minhkhoav47@gmail.com>
2026-03-19 20:19:49 +0700

    file code updated

--- lab2/Ngaythang.cpp
+++ lab2/Ngaythang.cpp
@@ -1,20 +1,12 @@
 #include <bits/stdc++.h>
 #include "Ngaythang.h"
 using namespace std;
-bool kiemtranam(int nam){
-    string s = to_string(nam);
-    for(char c : s){
-        if(!isdigit(c)){
-            return false;
-        }
-    }
-    return true;
-}
-// ham xac dinh nam nhuan
+
+// hàm xác định năm nhuận
 bool namnhuan(int nam){
     return (nam % 400 == 0 || (nam % 4 == 0 && nam % 100 != 0));
 }
-// ham xac dinh ngay theo dung thang
+// hàm xác định từng ngày theo tháng
 int ngaytheothang(int thang,int nam){
     int ngayarr[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if(namnhuan(nam) && thang == 2){
@@ -33,16 +25,20 @@ void Ngay::Nhap(){
     cout << "Thang 31 ngay : 1, 3, 5, 7, 8, 10, 12"<<endl;
     cout << "Thang 30 ngay : 4, 6, 9, 11"<<endl;
     cout<<"nhap ngay thang nam hien tai";
-    do{
-        int year;
-        cin>>iNgay>>iThang>>year;
-        if(cin.fail()){
-            cout<<"input khong hop le( khong cung kieu du lieu)";
-            cin.clear();
-            cin.ignore(500,'\n');
+    do{ 
+        string line; // Ở đây user sẽ cho 1 chuối trống trước 
+        char extra; // tao them 1 ki tu để kiểm tra trường hợp cuối kí tự cỏn lỗi hay bị thừa input sai không
+        getline(cin,line); // bắt đầu đọc hết chuỗi
+        stringstream ss(line); // sử dụng stringstream để đọc từng từ trong chuỗi line
+
+        if(!(ss>>iNgay>>iThang>>iNam) || ss >> extra){
+            // điều kiện kiểm tra từng kí tự của stringstream giúp kiểm tra chuỗi line xem nó có thỏa điều kiện không
+            // Nếu kiểm tra thỏa 3 số đầu thì nếu có input dư thì khi gặp 1 kí tự extra thì cho nó false 
+            cout<<"input khong hop le(sai kiểu định dạng hoặc sai kiểu dữ liệu)"<<endl;
+            cout<<"Nhap lai:";
             continue;
         }
-        if(year < 0){
+        if(iNam < 0){
             cout<<"Nam khong hop le"<<endl;
             continue;
         }
@@ -50,32 +46,37 @@ void Ngay::Nhap(){
             cout<<"thang khong hop le"<<endl;
             continue;
         }
-        if(iNgay <= 0 || iNgay >31){
+        if(iNgay <= 0 || iNgay > 31){
             cout<<"ngay khong hop le";
             continue;
         }
+        // xác định số ngày tối đa trong tháng đó, đảm bảo không vượt quá ngày theo tháng
         if(iNgay > ngaytheothang(iThang,iNam)){
+            if(namnhuan(iNam) && iThang == 2){
+                cout<<"Nam nhuan, thang 2 co 29 ngay"<<endl;
+            }
             cout<<"thang "<<iThang<<" co toi da "<<ngaytheothang(iThang,iNam)<<" ngay"<<endl;
             continue; 
         }
-        if(!kiemtranam(year)){
-            cout<<"nam khong hop le nhap lai";
-            continue;
-        }
-        iNam = year;
-        break; // neu vuot qua het tat ca cac dieu kien tren
+        break; // Nếu thỏa hết thì break khỏi vòng lặp do while để bước sang cộng ngày tiếp theo
 
     }while(true);
 }
-
+// Hàm NgayThangNamTiepTheo()
+// Đầu vào: Không có 
+// Đẩu ra: Trả về kết quả của ngày tiếp theo 
+// chức nang: Lấy dữ liệu từ ngày cũ thông qua biến con trỏ this lấy từ hàm gốc class ban đầu
+//            Sau đó đem di cộng từng phần theo ngày tháng năm 
 Ngay Ngay::NgayThangNamTiepTheo(){
-    Ngay ketqua = *this;
-    ketqua.iNgay++;
-    if(ketqua.iNgay > ngaytheothang(iThang,iNam)){
+    Ngay ketqua = *this; // tạo 1 hàm khởi tạo mói lấy class và lưu thông tin của ngày cũ
+    ketqua.iNgay++; 
+    if(ketqua.iNgay > ngaytheothang(ketqua.iThang,ketqua.iNam)){
+        // điều kiện nếu ngày tiếp theo của ketqua vượt quá ngày theo tháng cho phép
         ketqua.iNgay = 1;
         ketqua.iThang++;
     }
     if(ketqua.iThang > 12){
+        // điều kiện tháng vươt quá số quy định
         ketqua.iThang = 1;
         ketqua.iNam++;
     }
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,7 +3,7 @@
 #include "Ngaythang.cpp"
 using namespace std;
 int main(){
-    Ngay N1;
+    Ngay N1; // tạo input cho ngày 1
     cout<<"nhap input cho ngay thang nam: ";
     N1.Nhap();
     Ngay res = N1.NgayThangNamTiepTheo();
